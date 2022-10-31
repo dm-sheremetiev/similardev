60b0f7ac1a51fbfaa3e6728a7117835dc730d637
Dmytro Sheremetiev <dmytriy.sheremetiev@gmail.com>
2022-10-31 21:47:57 +0200

    First commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const registerButton = document.querySelector('.register-button');
+const registerBlock = document.querySelector('.register-block');
+const mainInfo = document.querySelector('.main-info');
+const header = document.querySelector('.header');
+
+registerButton.addEventListener('click', () => {
+  registerBlock.classList.remove('register-block--is-hidden');
+  mainInfo.classList.add('main-info--is-hidden');
+  header.classList.add('header--is-hidden');
+});
+
+const closeFormIcon = document.querySelector('.selector-form__close-form');
+
+closeFormIcon.addEventListener('click', () => {
+  registerBlock.classList.add('register-block--is-hidden');
+  mainInfo.classList.remove('main-info--is-hidden');
+  header.classList.remove('header--is-hidden');
+});
+
+const nextButton = document.querySelector('.button__next-button');
+const selectorFormFirst = document.querySelector('.selector-form--first');
+const selectorFormSecond = document.querySelector('.selector-form--second');
+
+nextButton.addEventListener('click', () => {
+  selectorFormFirst.classList.add('selector-form--is-hidden');
+  selectorFormSecond.classList.remove('selector-form--is-hidden');
+});
